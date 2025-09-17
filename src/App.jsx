68bb078dba79e011662372e00d562ce41e79611c
fahmidtasin <fahmidtasin@gmail.com>
2025-09-17 13:11:08 +0600

    feat: update initial app with navbar, clock, and counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date().toLocaleTimeString()), 1000)
+    return () => clearInterval(timer)
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <nav className='navbar'>
+        <ul className='nav-left'>
+          <li><a href="#" target="_blank"><img src={reactLogo} className="logo react" alt="React logo" /> </a></li>
+          <li><a href="#" target="_blank"><img src={viteLogo} className="logo" alt="Vite logo" /> </a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <ul className='nav-right'>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Sign Up</a></li>
+        </ul>
+      </nav>
+
+      {/* Clock */}
+      <div className="clock">
+        <p>{time}</p>
       </div>
-      <h1>Vite + React</h1>
+    
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </main>
   )
 }
 
 export default App
+
